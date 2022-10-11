658b23f950c722611a3b0f6d98bbbd27c6d4306b
Dmitri Sholomitski <d.sholomitski@outlook.com>
2022-10-11 14:25:57 +0300

    added comments and README

--- expertiments.py
+++ expertiments.py
@@ -56,6 +56,7 @@ def get_svo(doc: stanza.Document) -> list[Phrase]:
     svo = defaultdict(Phrase)
     _xcomp = defaultdict(Word)
     for sent in doc.sentences:
+
         for word_1, dep_type, word_2 in sent.dependencies:
             if word_1.pos != 'VERB' and word_2.pos != 'VERB':
                 continue
@@ -64,17 +65,20 @@ def get_svo(doc: stanza.Document) -> list[Phrase]:
             if dep_type in ('nsubj', 'obj') or (dep_type == 'advmod' and other.text == 'не'):
                 svo[(sent.index, verb.id)].add(dep_type, other)
                 svo[(sent.index, verb.id)].add('verb', verb)
+
         for word_1, dep_type, word_2 in sent.dependencies:
             if word_1.pos != 'VERB' and word_2.pos != 'VERB':
                 continue
             verb, other = (word_1, word_2) if word_1.pos == 'VERB' else (word_2, word_1)
 
             if dep_type == 'conj' and svo[(sent.index, other.id)].nsubj is None:
+                # "subj verb1 obj1 and verb2 obj2" -> [(subj, verb1, obj1), (subj, verb2, obj2)]
                 conj_nsubj = svo[(sent.index, verb.id)].nsubj
                 svo[(sent.index, other.id)].add('nsubj', conj_nsubj)
                 svo[(sent.index, other.id)].add('verb', other)
 
             elif dep_type == 'xcomp':
+                # "subj verb1 ... verbN objN" -> [(subj, verb1 + ... + verbN, objN)]
                 try:
                     initial_verb = _xcomp[(sent.index, verb.id)]
                 except:
