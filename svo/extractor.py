ca2bef368926eaa453f5a41e41766f2cdd95bcf9
Dmitri Sholomitski <d.sholomitski@outlook.com>
2022-10-12 10:12:07 +0300

    updated `conj` logic; renamed tests

--- svo/extractor.py
+++ svo/extractor.py
@@ -29,13 +29,7 @@ def extract_svo(doc: stanza.Document) -> list[Phrase]:
                 continue
             verb, other = (word_1, word_2) if word_1.pos == 'VERB' else (word_2, word_1)
 
-            if dep_type == 'conj' and svo[(sent.index, other.id)].nsubj is None:
-                # "subj verb1 obj1 and verb2 obj2" -> [(subj, verb1, obj1), (subj, verb2, obj2)]
-                conj_nsubj = svo[(sent.index, verb.id)].nsubj
-                svo[(sent.index, other.id)].add('nsubj', conj_nsubj)
-                svo[(sent.index, other.id)].add('verb', other)
-
-            elif dep_type == 'xcomp':
+            if dep_type == 'xcomp':
                 # "subj verb1 ... verbN objN" -> [(subj, verb1 + ... + verbN, objN)]
                 try:
                     initial_verb = _xcomp[(sent.index, verb.id)]
@@ -49,6 +43,23 @@ def extract_svo(doc: stanza.Document) -> list[Phrase]:
                     svo[(sent.index, initial_verb.id)].add('obj', other_obj)
                 except:
                     pass
+
+        for word_1, dep_type, word_2 in sent.dependencies:
+            if word_1.pos != 'VERB' and word_2.pos != 'VERB':
+                continue
+            verb, other = (word_1, word_2) if word_1.pos == 'VERB' else (word_2, word_1)
+
+            if dep_type == 'conj' and svo[(sent.index, other.id)].nsubj is None:
+                # "subj verb1 obj1 and verb2 obj2" -> [(subj, verb1, obj1), (subj, verb2, obj2)]
+                try:
+                    initial_verb = _xcomp[(sent.index, verb.id)]
+                except:
+                    initial_verb = verb
+                conj_nsubj = svo[(sent.index, initial_verb.id)].nsubj
+                svo[(sent.index, other.id)].add('nsubj', conj_nsubj)
+                svo[(sent.index, other.id)].add('verb', other)
+                if initial_verb != verb:
+                    svo[(sent.index, other.id)].add('verb', initial_verb)
 
     svo_list = [x for x in svo.values() if x.complete]
     return svo_list
